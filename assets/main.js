b6e3dc1065cfe41f94c94c0af15770a87cc279b9
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-04-01 15:45:41 -0400

    added css to the header to center it and added function to finding todays weather

--- assets/main.js
+++ assets/main.js
@@ -27,3 +27,23 @@ $(document).ready(function() {
     .format("MMMM Do");
   $("#fiveday").html(fiveDay);
 });
+
+var apiKey = "3c6fea3acc237375fd52d7b6d98475c0";
+var cityName = "Philadelphia";
+var onedayURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
+
+$.ajax({
+  url: onedayURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(onedayURL);
+  console.log(response);
+  console.log(response.weather[0].icon);
+  var iconCode = response.weather[0].icon;
+  var iconURL = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  $("#weatherIcon").attr("src", iconURL);
+  $("#city").html(response.name);
+  $("#temp").html("Temperature: " + response.main.temp);
+  $("#humidity").html("Humidity: " + response.main.humidity + "%");
+  $("#windSpeed").html("Wind Speed: " + response.wind.speed + " mph");
+});
